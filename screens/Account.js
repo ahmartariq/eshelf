b7324694a7cdceee9d3fc6bf01fa03a940cfa9ba
shahzaibkhursheed <iamzaibi905@gmail.com>
2023-01-05 00:40:20 +0500

    Toggle button added and completed userSetting Screen

--- screens/Account.js
+++ screens/Account.js
@@ -1,8 +1,9 @@
-import { Text, TextInput, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { Text, TextInput, View, Image, TouchableOpacity, ScrollView, Alert } from 'react-native';
 import Theme from '../Theme';
 import {SettingLink} from '../components/SettingLink';
 import { Field, Password } from '../components/Inputs';
 import { Button } from '../components/Button';
+import { useState } from 'react';
 
 const colors = Theme.colors
 const size = Theme.size
@@ -28,17 +29,21 @@ const Account = () => {
 
 export const ChangeEmail = () => {
 
+    const [currentPassword, setCurrentPassword] = useState('');
+    const [newEmail, setnNewEmail] = useState('');
+    const [retypeNewEmail, setRetypeNewEmail] = useState('');
+
     return(
         <View style={{ backgroundColor: colors.background , paddingTop: '10%', paddingHorizontal: 35 , flex: 1}}>
             <Image style={{marginTop:9, width:18, height:18 }} source={require("../assets/pics/backarrow.png")}></Image>
             <Text style={{marginBottom:12, marginTop:21, fontSize:28 ,color:'#F8EFEB', fontFamily: 'GT-America-Medium'}}>CHANGE EMAIL</Text>
             <Text style={{color:colors.gray, marginBottom:36 }}>YOUR CURRENT EMAIL ADDRESS <Text style={{fontWeight:'bold'}}>IAMZAIBI905@GMAIL.COM</Text></Text>
             
-            <Password placeholder={"CURRENT PASSWORD "}  />  
+            <Password placeholder={"CURRENT PASSWORD "} onChangeText={setCurrentPassword} value={currentPassword} />  
 
-            <Field placeholder={"NEW EMAIL"} marginTop={"7%"} />
+            <Field placeholder={"NEW EMAIL"} marginTop={"7%"} onChangeText={setnNewEmail} value={newEmail} />
             
-            <Field placeholder={"RETYPE NEW EMAIL"} marginTop={"7%"} />
+            <Field placeholder={"RETYPE NEW EMAIL"} marginTop={"7%"} onChangeText={setRetypeNewEmail} value={retypeNewEmail} />
 
             <Button height={50} text={"SAVE"} marginTop={"10%"} />
 
