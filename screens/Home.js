cd7baacd788509ad709ab3d9903d14e4ff0f2733
ahmar <ahmartariq46@yahoo.com>
2023-01-12 00:11:41 +0500

    firebase added and signup made

--- screens/Home.js
+++ screens/Home.js
@@ -1,24 +1,38 @@
 import React from 'react'
-import { View, Image , ScrollView , Text} from 'react-native'
-import Theme  from '../Theme'
+import { View, Image, ScrollView, Text } from 'react-native'
+import Theme from '../Theme'
+import Swiper from 'react-native-swiper'
 
 const Home1 = '../assets/pics/home1.png'
 const Home2 = '../assets/pics/home2.png'
 const Home3 = '../assets/pics/home3.png'
+const colors = Theme.colors
 
 
 const eshlefBlack = Theme.eshlefBlack
+
 export default Home = () => {
     return (
-        <ScrollView contentContainerStyle={{ flexGrow: 1 , position : 'relative' }}>
-        <Text style={eshlefBlack}>ESHELF</Text>
-            <Image source={require(Home1)}/>
-            <Image source={require(Home2)}/>
-            <Image source={require(Home3)}/>
-            <View style={{flex : 1 , height : 770 , justifyContent :'center' , alignItems : 'center' , position : 'relative'}}>
+        <View contentContainerStyle={{ flexGrow: 1, position: 'relative' }}>
+            <Text style={eshlefBlack}>ESHELF</Text>
+            <View style={{ width: '100%', height: '100%'}}>
+
+            <Swiper  horizontal={false} style={{ position: 'relative' }} showsButtons={false} dotColor={colors.background} activeDotColor={colors.background} dotStyle={{ width: 6, height: 6, marginRight: 4 }} activeDotStyle={{ borderWidth: 1, borderColor: colors.background }} paginationStyle={{ flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'flex-end', height: '100%', justifyContent: 'center', marginRight: 10 }}>
+                <View>
+                    <Image source={require(Home1)} />
+                </View>
+                <View>
+                    <Image source={require(Home2)} />
+                </View>
+                <View>
+                    <Image source={require(Home3)} />
+                </View>
+                <View style={{ flex: 1, height: 770, justifyContent: 'center', alignItems: 'center', position: 'relative' }}>
                     <Text style={Theme.title1Black}>JOIN OUR NEWS LETTER</Text>
-                    <Text style={{alignSelf : 'flex-start' , position : 'absolute' , bottom : 130 , left : 21 , fontSize : 14 , fontFamily : 'GT-America-Regular' }}>PRIVACY POLICY | TERMS OF USE</Text>
+                    <Text style={{ alignSelf: 'flex-start', position: 'absolute', bottom: 130, left: 21, fontSize: 14, fontFamily: 'GT-America-Regular' }}>PRIVACY POLICY | TERMS OF USE</Text>
+                </View>
+            </Swiper>
             </View>
-        </ScrollView>
+        </View>
     )
 }
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,50 +1,92 @@
-import { useState, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 import { Text, TextInput, View, Image, TouchableOpacity, ScrollView } from 'react-native';
 import Theme from '../Theme';
 import { Button } from '../components/Button';
 import { Field, Password } from '../components/Inputs';
+import { auth, db } from '../FirebaseConfig';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { ref } from "firebase/database";
+import { set } from 'firebase/database';
 
 
-
-const Facebook = '../assets/pics/facebook.png'
-const Instagram = '../assets/pics/instagram.png'
-const Twitter = '../assets/pics/twitter.png'
-
-
-const handleSignUp = () => {
-
-}
+const colors = Theme.colors
+const size = Theme.size
+const title1 = Theme.title1
+const text = Theme.text
 
 const SignUp = () => {
 
-    const colors = Theme.colors
-    const size = Theme.size
-    const title1 = Theme.title1
-    const text = Theme.text
-    return (
-        <View style={{ backgroundColor: colors.background, paddingTop: '10%', paddingHorizontal: 35, flex: 1 }}>
-            <Text style={title1}>Create an account</Text>
-            <Text style={{ fontSize: text.fontSize, color: text.color, marginTop: 14, fontFamily: text.fontFamily }}>Provice your information below to create a
-                new account.</Text>
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [reEmail, setReEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [rePassword, setRePassword] = useState('')
+  const [err, setErr] = useState('')
 
-            <Field marginTop={'15%'} placeholder={"NAME"} keyboardType={"text"} />
-            <Field marginTop={'10%'} placeholder={"EMAIL"} keyboardType={"email-address"} autoComplete={"email"} />
-            <Field marginTop={'10%'} placeholder={"RETYPE EMAIL"} keyboardType={"email-address"} autoComplete={"email"} />
-            <Password marginTop={"10%"} placeholder={"PASSWORD "} />
-            <Password marginTop={"10%"} placeholder={"RETYPE PASSWORD"} />
+  const handleSignUp = () => {
+    console.log("pressed");
+    setErr('')
+    if(name === '')
+      setErr('Name Field is empty')
+    else if(email === '')
+      setErr('Email Field is empty')
+    else if(reEmail === '')
+      setErr('Retype email Field is empty')
+    else if(email != reEmail)
+      setErr('Retype email does not match email')
+    else if(password === '')
+      setErr('Enter your password')
+    else if(rePassword === '')
+      setErr('Retype password Field is empty')
+    else if(password != rePassword)
+      setErr('Retype password does not match email')
+    else{
+      createUserWithEmailAndPassword(auth, email, password )
+      .then(userCredential => {
+        writeUserData(email, name, userCredential.user.uid);
+        // navigation.navigate('Login');
+        setErr("")
+      })
+      .catch(error => {
+        if(error = 'Password should be at least 6 characters (auth/weak-password)') setErr('Password should be at least 6 characters')
+        else{
+          setErr("Email address already in use");
+        }
+      });
+    }
+  }
 
-            <View style={{marginTop: '20%' , justifyContent: 'center' }}>
+  const writeUserData = (email, name,id) => {
+    set(ref(db, 'users/' + id), {
+      name: name,
+      email: email,
+      
+    });
+    console.log("here");
+  }
 
-            <View style={{ flexDirection: 'row' , justifyContent: 'center'}}>
-                <Text style={{ color: colors.text }}>Don't have an account? </Text>
-                <TouchableOpacity activeOpacity={0.7}>
-                    <Text style={{ color: colors.tertiary, textDecorationLine: 'underline' }}>Register</Text>
-                </TouchableOpacity>
-            </View>
-            <Button height={54} marginTop={"3%"} text={"Login"} onPress={handleSignUp} />
-            </View>
+  return (
+    <View style={{ backgroundColor: colors.background, paddingTop: '10%', paddingHorizontal: 35, flex: 1 }}>
+      <Text style={title1}>Create an account</Text>
+      <Text style={{ fontSize: text.fontSize, color: text.color, marginTop: 14, fontFamily: text.fontFamily }}>Provice your information below to create a
+        new account.</Text>
+      <Field marginTop={'15%'} placeholder={"NAME"} keyboardType={"text"} value={name} onChangeText={(text) => setName(text)} />
+      <Field marginTop={'10%'} placeholder={"EMAIL"} keyboardType={"email-address"} autoComplete={"email"} value={email} onChangeText={(text) => setEmail(text)} />
+      <Field marginTop={'10%'} placeholder={"RETYPE EMAIL"} keyboardType={"email-address"} autoComplete={"email"} value={reEmail} onChangeText={(text) => setReEmail(text)} />
+      <Password marginTop={"10%"} placeholder={"PASSWORD "} value={password} onChangeText={(text) => setPassword(text)} />
+      <Password marginTop={"10%"} placeholder={"RETYPE PASSWORD"} value={rePassword} onChangeText={(text) => setRePassword(text)} />
+    <Text style={{marginTop: "2%" , color: 'red' , fontSize: size.text}}>{err}</Text>
+      <View style={{ marginTop: '20%', justifyContent: 'center' }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+          <Text style={{ color: colors.text }}>Don't have an account? </Text>
+          <TouchableOpacity activeOpacity={0.7}>
+            <Text style={{ color: colors.tertiary, textDecorationLine: 'underline' }}>Register</Text>
+          </TouchableOpacity>
         </View>
-    )
+        <Button height={54} marginTop={"3%"} text={"Login"} onPress={handleSignUp} />
+      </View>
+    </View>
+  )
 }
 
 
